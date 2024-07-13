2b35648461762cf1b5aafd99db15268474d4a8ff
tentenball <lth8905@gmail.com>
2024-07-14 00:35:51 +0900

    first commit

---
+++ src/main/java/gible/GibleApplication.java
@@ -0,0 +1,13 @@
+package gible;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GibleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gible/GibleApplicationTests.java
@@ -0,0 +1,13 @@
+package gible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GibleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
