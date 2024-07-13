3c1d1f6ee972176cbfa0f575d1f9506e6dd8b48e
tentenball <lth8905@gmail.com>
2024-07-14 01:29:39 +0900

    feat: GlobalExceptionHandler, CustomException 함수 작성

--- src/main/java/gible/exception/GlobalExceptionHandler.java
+++ src/main/java/gible/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package gible.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,4 +11,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<?> handle(CustomException ex){
+        ErrorType errortype = ex.getErrorCode();
+        log.warn("Error occurred : [errorCode={}, message={}]", errortype.getStatus(), errortype.getMessage());
+        return ResponseEntity.status(errortype.getStatus()).body(ErrorDto.of(errortype.getStatus(), errortype.getMessage()));
+    }
 }
