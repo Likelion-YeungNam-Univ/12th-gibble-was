69c88bd01637ea2e35d80dce2ce9f7d26eb77cbb
DaeYoung Park <pdy000726@naver.com>
2024-07-14 17:52:52 +0900

    feat: User에 대한 Role eunm class 작성

--- src/main/java/gible/domain/user/entity/Role.java
+++ src/main/java/gible/domain/user/entity/Role.java
@@ -1,2 +1,12 @@
-package gible.domain.user.entity;public enum Role {
+package gible.domain.user.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
+    private final String value;
 }
