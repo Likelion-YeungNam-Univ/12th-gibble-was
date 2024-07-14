7d1af36d01f00d8444ef3b2c3adb217922f0dc4b
DaeYoung Park <pdy000726@naver.com>
2024-07-14 17:53:41 +0900

    feat: JpaAuditing을 위한 config 작성

--- src/main/java/gible/config/JpaConfig.java
+++ src/main/java/gible/config/JpaConfig.java
@@ -1,2 +1,9 @@
-package gible.config;public class JpaConfig {
+package gible.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
 }
