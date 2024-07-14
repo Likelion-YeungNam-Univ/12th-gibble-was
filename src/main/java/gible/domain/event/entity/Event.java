34723e7a07f2f25c64236bf38e87587f119943a2
DaeYoung Park <pdy000726@naver.com>
2024-07-14 17:53:58 +0900

    feat: Event Entity 작성

--- src/main/java/gible/domain/event/entity/Event.java
+++ src/main/java/gible/domain/event/entity/Event.java
@@ -1,2 +1,41 @@
-package gible.domain.event.entity;public class Event {
+package gible.domain.event.entity;
+
+import gible.domain.participate.entity.Participate;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @Column(name = "image_url")
+    private String imageUrl;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.REMOVE)
+    private List<Participate> participates;
+
+    @Builder
+    public Event(String title, String content, String imageUrl) {
+        this.title = title;
+        this.content = content;
+        this.imageUrl = imageUrl;
+    }
 }
