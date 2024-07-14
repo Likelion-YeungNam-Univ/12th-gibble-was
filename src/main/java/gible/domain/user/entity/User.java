340c9125f8025167c96ff6dc46668073749c5d97
DaeYoung Park <pdy000726@naver.com>
2024-07-14 17:52:13 +0900

    feat: User Entity 작성

--- src/main/java/gible/domain/user/entity/User.java
+++ src/main/java/gible/domain/user/entity/User.java
@@ -1,2 +1,65 @@
-package gible.domain.user.entity;public class User {
+package gible.domain.user.entity;
+
+import gible.domain.donation.entity.Donation;
+import gible.domain.participate.entity.Participate;
+import gible.domain.post.entity.Post;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Column(unique = true)
+    private String nickname;
+
+    @NotNull
+    @Column(name = "phone_number", unique = true)
+    private String phoneNumber;
+
+    @Column(name = "donate_count")
+    private int donateCount;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @OneToMany(mappedBy = "writer", cascade = CascadeType.REMOVE)
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.REMOVE)
+    private List<Donation> donationsSent;
+
+    @OneToMany(mappedBy = "receiver", cascade = CascadeType.REMOVE)
+    private List<Donation> donationsReceived;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private List<Participate> participates;
+
+    @Builder
+    public User(String name, String email, String nickname, String phoneNumber, Role role) {
+        this.name = name;
+        this.email = email;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
 }
