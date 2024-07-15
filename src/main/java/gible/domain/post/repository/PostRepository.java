0dbb2b3c48ba3c1f2d87dbd8b0ea04a740dd8cc9
DaeYoung Park <pdy000726@naver.com>
2024-07-15 19:50:46 +0900

    feat: 게시글 검색을 위한 jpa 네이밍 메서드 추가

--- src/main/java/gible/domain/post/repository/PostRepository.java
+++ src/main/java/gible/domain/post/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package gible.domain.post.repository;
 
 import gible.domain.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
+
+    Page<Post> findByTitleContaining(String search, Pageable pageable);
 }
