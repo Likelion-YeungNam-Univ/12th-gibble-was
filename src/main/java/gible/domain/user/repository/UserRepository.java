b411028e184fac320765cad1e85a22ce2a8d52ba
tentenball <lth8905@gmail.com>
2024-07-15 17:41:10 +0900

    hotfix: UserRepository 제네릭 오류 수정

--- src/main/java/gible/domain/user/repository/UserRepository.java
+++ src/main/java/gible/domain/user/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<UUID, User> {
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
