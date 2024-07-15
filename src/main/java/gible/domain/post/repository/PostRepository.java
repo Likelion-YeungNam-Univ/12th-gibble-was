873b6233c00dd5405ab347598496a4a27c0f3d86
DaeYoung Park <pdy000726@naver.com>
2024-07-15 17:42:29 +0900

    feat: post repository 작성

--- src/main/java/gible/domain/post/repository/PostRepository.java
+++ src/main/java/gible/domain/post/repository/PostRepository.java
@@ -1,2 +1,9 @@
-package gible.domain.post.repository;public interface PostRepository {
+package gible.domain.post.repository;
+
+import gible.domain.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PostRepository extends JpaRepository<Post, UUID> {
 }
