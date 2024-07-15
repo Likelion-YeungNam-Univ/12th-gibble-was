801a1fd7bad027efb087a817afb03be6307cb8e7
DaeYoung Park <pdy000726@naver.com>
2024-07-15 19:51:27 +0900

    feat: PostService 구현

--- src/main/java/gible/domain/post/service/PostService.java
+++ src/main/java/gible/domain/post/service/PostService.java
@@ -1,2 +1,83 @@
-package gible.domain.post.service;public class PostService {
+package gible.domain.post.service;
+
+import gible.domain.post.dto.PostDetailRes;
+import gible.domain.post.dto.PostReq;
+import gible.domain.post.dto.PostSummaryRes;
+import gible.domain.post.entity.Post;
+import gible.domain.post.repository.PostRepository;
+import gible.domain.user.entity.User;
+import gible.domain.user.repository.UserRepository;
+import gible.exception.CustomException;
+import gible.exception.error.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final UserRepository userRepository;
+    private final PostRepository postRepository;
+
+    /* 게시글 생성 */
+    @Transactional
+    public void savePost(PostReq postReq, UUID userId) {
+
+        User foundUser = userRepository.findById(userId).orElseThrow(() ->
+                new CustomException(ErrorType.USER_NOT_FOUND));
+
+        Post post = postReq.toEntity(postReq, foundUser);
+
+        postRepository.save(post);
+    }
+
+    /* 전체 게시글 불러오기 */
+    @Transactional(readOnly = true)
+    public Page<PostSummaryRes> getAllPosts(Pageable pageable) {
+
+        Page<Post> posts = postRepository.findAll(pageable);
+        return posts.map(PostSummaryRes::fromEntity);
+    }
+
+    /* 특정 게시글 불러오기 */
+    @Transactional(readOnly = true)
+    public PostDetailRes getPost(UUID postId) {
+
+        Post foundPost = postRepository.findById(postId).orElseThrow(() ->
+                new CustomException(ErrorType.POST_NOT_FOUND));
+        return PostDetailRes.fromEntity(foundPost);
+    }
+
+    /* 검색한 단어에 대한 게시글 불러오기 */
+    @Transactional(readOnly = true)
+    public Page<PostSummaryRes> getPostsByKeyword(String search, Pageable pageable) {
+
+        Page<Post> searchPosts = postRepository.findByTitleContaining(search, pageable);
+        return searchPosts.map(PostSummaryRes::fromEntity);
+    }
+
+    /* 게시글 수정 */
+    @Transactional
+    public void updatePost(PostReq postReq, UUID postId) {
+
+        Post foundPost = postRepository.findById(postId).orElseThrow(() ->
+                new CustomException(ErrorType.POST_NOT_FOUND));
+
+        foundPost.updatePost(postReq);
+    }
+
+    /* 게시글 삭제 */
+    @Transactional
+    public void deletePost(UUID postId) {
+
+        Post foundPost = postRepository.findById(postId).orElseThrow(() ->
+                new CustomException(ErrorType.POST_NOT_FOUND));
+
+        postRepository.delete(foundPost);
+    }
 }
