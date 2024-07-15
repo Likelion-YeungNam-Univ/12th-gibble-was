603884df27414975e9664ab2632b735315c94be3
tentenball <lth8905@gmail.com>
2024-07-15 17:20:38 +0900

    feat: UserRepository 작성

---
+++ src/main/java/gible/domain/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package gible.domain.user.repository;
+
+import gible.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<UUID, User> {
+}
