d98ae5d27a4fb0983fdedc096513085804b557d9
DaeYoung Park <pdy000726@naver.com>
2024-07-15 15:11:01 +0900

    refact: ROLE 접두사 삭제

--- src/main/java/gible/domain/user/entity/Role.java
+++ src/main/java/gible/domain/user/entity/Role.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum Role {
-    USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+    USER("USER"), ADMIN("ADMIN");
 
     private final String value;
 }
