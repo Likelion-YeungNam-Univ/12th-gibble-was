451eee87b33735f7843eac16eb9fc6ece1a09cf9
DaeYoung Park <pdy000726@naver.com>
2024-07-15 22:02:43 +0900

    feat: api 공통 응답 포맷

--- src/main/java/gible/global/util/api/ApiUtil.java
+++ src/main/java/gible/global/util/api/ApiUtil.java
@@ -1,2 +1,21 @@
-package gible.global.util.api;public class ApiUtil {
+package gible.global.util.api;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class ApiUtil<T> {
+
+    private T response;
+
+    @Builder
+    private ApiUtil(T response) {
+        this.response = response;
+    }
+
+    public static <T> ApiUtil<T> from(T response) {
+        return ApiUtil.<T>builder()
+                .response(response)
+                .build();
+    }
 }
