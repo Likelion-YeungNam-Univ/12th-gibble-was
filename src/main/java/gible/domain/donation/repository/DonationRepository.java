0312f55a16238fae943107140829ed336c39a738
DaeYoung Park <pdy000726@naver.com>
2024-07-16 17:12:39 +0900

    feat: Donation Repository 작성

--- src/main/java/gible/domain/donation/repository/DonationRepository.java
+++ src/main/java/gible/domain/donation/repository/DonationRepository.java
@@ -1,2 +1,9 @@
-package gible.domain.donation.repository;public interface DonationRepository {
+package gible.domain.donation.repository;
+
+import gible.domain.donation.entity.Donation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface DonationRepository extends JpaRepository<Donation, UUID> {
 }
