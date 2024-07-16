eb6e8a47542550e30ded17ebd57d564bb0a85b8b
DaeYoung Park <pdy000726@naver.com>
2024-07-16 19:48:52 +0900

    feat: DonationController 작성

---
+++ src/main/java/gible/domain/donation/controller/DonationController.java
@@ -0,0 +1,56 @@
+package gible.domain.donation.controller;
+
+import gible.domain.donation.dto.DonationPostInfoRes;
+import gible.domain.donation.dto.DonationReq;
+import gible.domain.donation.dto.DonationSenderInfoRes;
+import gible.domain.donation.service.DonationService;
+import gible.global.util.api.ApiUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RestController
+public class DonationController {
+
+    private final DonationService donationService;
+
+    /* 기부하기 */
+    @PostMapping("/post/{postId}/donate")
+    public ResponseEntity<?> donate(@Valid @RequestBody DonationReq donationReq,
+                                    @PathVariable UUID postId,
+                                    @AuthenticationPrincipal UserDetails userDetails) {
+        // 임시로 userDetails.getUsername()을 UUID로 바꿈. 추후에 auth 완료되면 수정필요.
+        donationService.donate(donationReq, UUID.fromString(userDetails.getUsername()), postId);
+        return ResponseEntity.ok(ApiUtil.from("기부 성공."));
+    }
+
+    /* 게시글에 대한 기부자 목록 불러오기 */
+    @GetMapping("/post/{postId}/donators")
+    public List<DonationSenderInfoRes> getDonorsForPost(@PathVariable UUID postId) {
+
+        return donationService.getDonorsForPost(postId);
+    }
+
+    /* 기부한 게시글에 대한 정보 불러오기 */
+    @GetMapping("/donation/my-donation")
+    public List<DonationPostInfoRes> getPostDonationDetails(
+            @AuthenticationPrincipal UserDetails userDetails) {
+        // 임시로 userDetails.getUsername()을 UUID로 바꿈. 추후에 auth 완료되면 수정필요.
+        return donationService.getPostDonationDetails(UUID.fromString(userDetails.getUsername()));
+    }
+
+    /* 기부해준 사람들의 목록 불러오기 */
+    @GetMapping("/donation/received-donation")
+    public List<DonationSenderInfoRes> getDonorsList(
+            @AuthenticationPrincipal UserDetails userDetails) {
+        // 임시로 userDetails.getUsername()을 UUID로 바꿈. 추후에 auth 완료되면 수정필요.
+        return donationService.getDonorsList(UUID.fromString(userDetails.getUsername()));
+    }
+}
