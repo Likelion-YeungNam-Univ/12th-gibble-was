c1a11ce88cc00079b0c3cbe0a24c16866cf657d7
tentenball <lth8905@gmail.com>
2024-07-16 13:52:25 +0900

    chore, feat: validateToken 구현, userSerivce 일부 구현, jjwt 디펜던시 추가

--- src/main/java/gible/domain/security/jwt/JwtTokenProvider.java
+++ src/main/java/gible/domain/security/jwt/JwtTokenProvider.java
@@ -1,15 +1,60 @@
 package gible.domain.security.jwt;
 
+
+import gible.domain.user.service.UserService;
+import gible.exception.CustomException;
+import gible.exception.error.ErrorType;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
+import java.util.UUID;
+
 @Component
 public class JwtTokenProvider {
-    public boolean validateToken(String token) {
+    private final SecretKey secretKey;
+    private final long expiration;
+    private final String issuer;
+    private final UserService userService;
+    public JwtTokenProvider(
+    @Value("${jwt.secret}") final String SECERT_KEY,
+    @Value("${jwt.expiration}") final long EXPIRATION,
+    @Value("${jwt.issuer}") final String ISSUER,
+    final UserService userService
+    ){
+        this.secretKey = new SecretKeySpec(SECERT_KEY.getBytes(), Jwts.SIG.HS256.key().build().getAlgorithm());
+        this.expiration = EXPIRATION;
+        this.issuer = ISSUER;
+        this.userService = userService;
+    }
 
+
+    public boolean validateToken(String token) {
+        Claims claims = parseClaims(token);
+
+        if(userService.findById(claims.get("auth", UUID.class)) == null){
+            throw new CustomException(ErrorType.USER_NOT_FOUND);
+        }
+        if(!claims.getExpiration().before(new Date())){
+            return true;
+        }
+        return false;
     }
 
     public Authentication getAuthentication(String token) {
+        
+    }
 
+    private Claims parseClaims(String accessToken){
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(accessToken)
+                .getPayload();
     }
 }
