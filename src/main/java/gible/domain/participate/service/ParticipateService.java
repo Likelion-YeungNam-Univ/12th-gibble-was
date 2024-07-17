3efe068ae526cfbfa46debb54a52e365b81c50c2
DaeYoung Park <pdy000726@naver.com>
2024-07-17 23:38:23 +0900

    feat: 사용자가 참여한 이벤트 조회 메서드 추가

--- src/main/java/gible/domain/participate/service/ParticipateService.java
+++ src/main/java/gible/domain/participate/service/ParticipateService.java
@@ -2,17 +2,20 @@ package gible.domain.participate.service;
 
 import gible.domain.event.entity.Event;
 import gible.domain.event.repository.EventRepository;
+import gible.domain.participate.dto.ParticipationEventRes;
 import gible.domain.participate.entity.Participate;
 import gible.domain.participate.repository.ParticipateRepository;
 import gible.domain.user.entity.User;
 import gible.domain.user.repository.UserRepository;
 import gible.exception.CustomException;
 import gible.exception.error.ErrorType;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +39,14 @@ public class ParticipateService {
                 .user(foundUser)
                 .event(foundEvent)
                 .build());
+    }
+
+    /* 참여한 이벤트 조회 */
+    @Transactional(readOnly = true)
+    public List<ParticipationEventRes> getAllParticipationEvents(String email) {
+
+        return participateRepository.findByUser_Email(email)
+                .stream().map(ParticipationEventRes::fromEntity)
+                .collect(Collectors.toList());
     }
 }
