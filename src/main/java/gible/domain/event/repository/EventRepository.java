a275383bbdfbb80a1a288f047dc1e9cf8e0f7e64
DaeYoung Park <pdy000726@naver.com>
2024-07-17 16:09:26 +0900

    feat: Event Repository 작성

--- src/main/java/gible/domain/event/repository/EventRepository.java
+++ src/main/java/gible/domain/event/repository/EventRepository.java
@@ -1,2 +1,9 @@
-package gible.domain.event.repository;public interface EventRepository {
+package gible.domain.event.repository;
+
+import gible.domain.event.entity.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
 }
