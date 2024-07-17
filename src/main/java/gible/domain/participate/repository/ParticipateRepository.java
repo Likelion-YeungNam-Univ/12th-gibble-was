11dbccb92b4c8e3ce3e1aee752802a83ddf49d22
DaeYoung Park <pdy000726@naver.com>
2024-07-17 23:37:55 +0900

    feat: 사용자의 이메일로 정보를 불러오기 위한 jpa 네이밍 메서드 추가

--- src/main/java/gible/domain/participate/repository/ParticipateRepository.java
+++ src/main/java/gible/domain/participate/repository/ParticipateRepository.java
@@ -3,7 +3,10 @@ package gible.domain.participate.repository;
 import gible.domain.participate.entity.Participate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ParticipateRepository extends JpaRepository<Participate, UUID> {
+
+    List<Participate> findByUser_Email(String email);
 }
