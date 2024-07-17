16b72cae6681e76bfe61dc383be2f38f4c4ae4f1
DaeYoung Park <pdy000726@naver.com>
2024-07-17 19:16:38 +0900

    feat: Event Controller 작성

--- src/main/java/gible/domain/event/controller/EventController.java
+++ src/main/java/gible/domain/event/controller/EventController.java
@@ -1,2 +1,65 @@
-package gible.domain.event.controller;public class EventController {
+package gible.domain.event.controller;
+
+import gible.domain.event.dto.EventDetailRes;
+import gible.domain.event.dto.EventReq;
+import gible.domain.event.dto.EventSummaryRes;
+import gible.domain.event.service.EventService;
+import gible.global.util.api.ApiUtil;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@RequestMapping("/event")
+@RestController
+public class EventController {
+
+    private final EventService eventService;
+
+    /* 이벤트 등록 */
+    @PostMapping("/upload")
+    public ResponseEntity<?> saveEvent(@Valid @RequestBody EventReq eventReq) {
+
+        eventService.saveEvent(eventReq);
+        return ResponseEntity.created(null).body(ApiUtil.from("이벤트 작성 성공."));
+    }
+
+    /* 이벤트 목록 조회 */
+    @GetMapping
+    public Page<EventSummaryRes> getAllEvents(
+            @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        return eventService.getAllEvents(pageable);
+    }
+
+    /* 특정 이벤트 조회 */
+    @GetMapping("/{eventId}")
+    public EventDetailRes getEvent(@PathVariable UUID eventId) {
+
+        return eventService.getEvent(eventId);
+    }
+
+    /* 이벤트 수정 */
+    @PutMapping("/upload/{eventId}")
+    public ResponseEntity<?> updateEvent(@Valid @RequestBody EventReq updateEventReq,
+                                         @PathVariable UUID eventId) {
+
+        eventService.updateEvent(updateEventReq, eventId);
+        return ResponseEntity.ok(ApiUtil.from("이벤트 수정 성공."));
+    }
+
+    /* 이벤트 삭제 */
+    @DeleteMapping("/{eventId}")
+    public ResponseEntity<?> deleteEvent(@PathVariable UUID eventId) {
+
+        eventService.deleteEvent(eventId);
+        return ResponseEntity.ok(ApiUtil.from("이벤트 삭제 성공."));
+    }
 }
