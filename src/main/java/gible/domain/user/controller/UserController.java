98a57ce25659b8c934a2bf36941af931ba5394b0
tentenball <lth8905@gmail.com>
2024-07-19 15:13:25 +0900

    feat: 유저컨트롤러 생성

---
+++ src/main/java/gible/domain/user/controller/UserController.java
@@ -0,0 +1,15 @@
+package gible.domain.user.controller;
+
+import gible.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+    private final UserService userService;
+
+
+}
