747129480bb9e4b2d1f372fc0e8e52e879b055f1
DaeYoung Park <pdy000726@naver.com>
2024-07-19 22:35:00 +0900

    feat: 게시글 작성자 pk값을 들고오기 위한 JPQL 작성

--- src/main/java/gible/domain/post/repository/PostRepository.java
+++ src/main/java/gible/domain/post/repository/PostRepository.java
@@ -1,13 +1,18 @@
 package gible.domain.post.repository;
 
 import gible.domain.post.entity.Post;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface PostRepository extends JpaRepository<Post, UUID> {
 
     Page<Post> findByTitleContaining(String search, Pageable pageable);
+
+    @Query("SELECT p.writer.id FROM Post p WHERE p.id = :postId")
+    UUID findWriterIdByPostId(@Param("postId") UUID postId);
 }
