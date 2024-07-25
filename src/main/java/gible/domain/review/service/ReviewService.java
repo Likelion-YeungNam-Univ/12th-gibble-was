1eb8043ec94750dc9cfc9a75ca0eb124acf3686c
tentenball <lth8905@gmail.com>
2024-07-26 00:44:43 +0900

    feat: controller, service 구현

---
+++ src/main/java/gible/domain/review/service/ReviewService.java
@@ -0,0 +1,46 @@
+package gible.domain.review.service;
+
+import gible.domain.review.dto.ReviewReq;
+import gible.domain.review.dto.ReviewRes;
+import gible.domain.review.entity.Review;
+import gible.domain.review.repository.ReviewRepository;
+import gible.domain.user.entity.User;
+import gible.domain.user.service.UserService;
+import gible.exception.CustomException;
+import gible.exception.error.ErrorType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ReviewService {
+    private final ReviewRepository reviewRepository;
+    private final UserService userService;
+
+    @Transactional(readOnly = true)
+    public List<ReviewRes> getReviews(Pageable pageable) {
+        return reviewRepository.findAll(pageable).stream().map(ReviewRes::fromEntity).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ReviewRes getReview(UUID reviewId) {
+        return reviewRepository.findById(reviewId).map(ReviewRes::fromEntity)
+                .orElseThrow(()-> new CustomException(ErrorType.EVENT_NOT_FOUND));
+    }
+
+    @Transactional
+    public void uploadReview(UUID userId, ReviewReq reviewReq) {
+        User user = userService.findById(userId);
+        reviewRepository.save(ReviewReq.toEntity(reviewReq, user));
+    }
+
+    @Transactional
+    public void deleteReview(UUID reviewId) {
+        reviewRepository.deleteById(reviewId);
+    }
+}
