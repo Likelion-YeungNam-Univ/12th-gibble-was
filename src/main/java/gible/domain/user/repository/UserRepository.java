095e9c996540fa8ba0d4aef211679efbe92d8f63
DaeYoung Park <pdy000726@naver.com>
2024-07-25 16:52:54 +0900

    feat: 메일 수신 동의 사용자 불러오는 JPA 네이밍 메서드 추가

--- src/main/java/gible/domain/user/repository/UserRepository.java
+++ src/main/java/gible/domain/user/repository/UserRepository.java
@@ -3,6 +3,7 @@ package gible.domain.user.repository;
 import gible.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,4 +11,6 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    List<User> findByEmailAgree(boolean emailAgree);
 }
