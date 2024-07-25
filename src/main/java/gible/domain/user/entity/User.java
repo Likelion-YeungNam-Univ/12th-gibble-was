f4eda2b58e16b93ef83393b3b4ae0dc6184a700c
박대영 <125343432+DaeYoung0726@users.noreply.github.com>
2024-07-25 23:24:41 +0900

    HF-102: 메일 서비스 구축 (#19)
    * feat: 메일 서비스 구축
    
    * feat: 메일 수신 동의 필드 추가
    
    * feat: 메일 수신 동의 사용자 불러오는 JPA 네이밍 메서드 추가
    
    * feat: 게시글 등록 시 메일 발송 로직 추가
    
    * test: MailService Test 작성
    
    * feat: 비동기 처리를 위한 어노테이션 추가
    
    * refact: point 필드 삭제

--- src/main/java/gible/domain/user/entity/User.java
+++ src/main/java/gible/domain/user/entity/User.java
@@ -10,6 +10,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,9 +41,11 @@ public class User {
     private String phoneNumber;
 
     @Column(name = "donate_count")
+    @ColumnDefault("0")
     private int donateCount;
 
-    private int point;
+    @Column(name = "email_agree")
+    private boolean emailAgree;
 
     @Enumerated(EnumType.STRING)
     private Role role;
@@ -60,11 +63,12 @@ public class User {
     private List<Participate> participates = new ArrayList<>();
 
     @Builder
-    public User(String name, String email, String nickname, String phoneNumber, Role role) {
+    public User(String name, String email, String nickname, String phoneNumber, boolean emailAgree, Role role) {
         this.name = name;
         this.email = email;
         this.nickname = nickname;
         this.phoneNumber = phoneNumber;
+        this.emailAgree = emailAgree;
         this.role = role;
     }
 }
