b481348d8250517e689b938177e4522cdfa140a0
tentenball <lth8905@gmail.com>
2024-07-28 16:46:46 +0900

    refact: 토근검증함수 validateToken 필터로 이동

--- src/main/java/gible/domain/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/gible/domain/security/jwt/JwtAuthenticationFilter.java
@@ -1,11 +1,15 @@
 package gible.domain.security.jwt;
 
+import gible.exception.CustomException;
+import gible.exception.error.ErrorType;
+import gible.global.common.jwt.AccessTokenProvider;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,17 +19,18 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-    private final JwtTokenProvider jwtTokenProvider;
+    private final AccessTokenProvider accessTokenProvider;
     private final UserDetailsService userDetailsService;
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = resolveToken(request);
 
-        if(token != null && jwtTokenProvider.validateToken(token)){
+        if(token != null && validateToken(token)){
             Authentication authentication = getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
@@ -33,18 +38,29 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     public Authentication getAuthentication(String token) {
-        Claims claims = jwtTokenProvider.parseClaims(token);
+        Claims claims = accessTokenProvider.parseClaims(token);
         String email = claims.getSubject();
         UserDetails userDetails = userDetailsService.loadUserByUsername(email);
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
+
     private String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
         if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7);
         }
         return null;
     }
 
+    public boolean validateToken(String token) {
+        Claims claims = accessTokenProvider.parseClaims(token);
+        if(claims == null){
+            throw new CustomException(ErrorType.INVALID_TOKEN);
+        }
+        if(claims.getExpiration().before(new Date())){
+            throw new CustomException(ErrorType.TOKEN_EXPIRED);
+        }
+        return !claims.getExpiration().before(new Date());
+    }
 
 }
