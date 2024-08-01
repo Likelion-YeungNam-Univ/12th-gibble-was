2439628c70cd711a895642496f98642a2da18385
tentenball <lth8905@gmail.com>
2024-08-01 11:22:31 +0900

    fix: 필요없는 빈 삭제

--- src/main/java/gible/config/SecurityConfig.java
+++ src/main/java/gible/config/SecurityConfig.java
@@ -5,8 +5,6 @@ import gible.domain.security.jwt.JwtAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 @Configuration
@@ -16,11 +14,5 @@ public class SecurityConfig {
     public JwtAuthenticationEntryPoint authenticationEntryPoint() {
         return new JwtAuthenticationEntryPoint(objectMapper);
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
-    }
-
 
 }
